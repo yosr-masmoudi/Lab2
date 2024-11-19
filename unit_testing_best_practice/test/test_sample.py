81a740022c803304b9a6403f2d237d51458f1ffa
yosr_masmoudi <yosrmasmoudi43@gmail.com>
2024-11-19 15:38:17 +0100

    Add the two folders

---
+++ unit_testing_best_practice/test/test_sample.py
@@ -0,0 +1,10 @@
+import sys
+# Always run from unit_testing_best_practice/test
+sys.path += ['C:/Users/yosrm/unit_testing_best_practice/src'] 
+
+from sample import *
+
+
+def test_answer():
+    assert func(3) == 5
+    
